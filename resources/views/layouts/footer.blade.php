b6fef9a1feea7a62999926078a3380808d238483
verenregina <reginaveren62@gmail.com>
2026-05-13 13:51:24 +0700

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,14 +1,18 @@
 <footer class="footer">
-    <div class="container footer-wrapper">
+    <div class="footer-wrapper">
 
-        <div class="footer-left">
-            <div class="logo">
+        <div class="footer-top">
+            <div class="logo-center">
                 <img src="{{ asset('assets/images/logo.png') }}" alt="logo">
             </div>
+            <div class="footer-line"></div>
+            
             <p>Sistem rekomendasi usaha berbasis data untuk membantu UMKM berkembang.</p>
         </div>
 
-        <div class="footer-right">
+        <div class="footer-line"></div>
+
+        <div class="footer-bottom">
             <p>© {{ date('Y') }} USAHAKU. All rights reserved.</p>
         </div>
 
